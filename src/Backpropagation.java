57c4c62ac46a5f63a540d0db64b9c22265342aaa
irotochukwusamuel <iro.tochukwu@umba.com>
2026-03-26 06:37:20 +0000

    feat: implement gradient calculations for hidden layers in backpropagation

--- src/Backpropagation.java
+++ src/Backpropagation.java
@@ -4,13 +4,34 @@ public class Backpropagation {
         return predicted_value - true_value;
     }
 
-    public double gradient_output_layer(double output_gradient, double[] weights){
-        double result = 0;
-        for(double v : weights){
-            result += v * output_gradient;
+    public double[] gradient_output_weights(double output_gradient, double[] weights){
+        double[] result = new double[weights.length];
+        for(int i = 0; i < weights.length; i++){
+            result[i] = output_gradient * weights[i];
         }
         return result;
     }
+
+    public double[] backdrop_hidden_layer(double output_gradient, double[] weights, double[] z){
+        double[] result = new double[weights.length];
+
+        for(int i = 0; i < weights.length; i++){
+            result[i] = (output_gradient * weights[i]) * Math.max(0, z[i]);
+        }
+        return result;
+    }
+
+    public double[][] gradients_hidden_Layer(double[] backdrop_hidden_layer, double[] inputs){
+        double[][] results = new double[backdrop_hidden_layer.length][inputs.length];
+        
+        for (int i = 0; i < backdrop_hidden_layer.length; i++) {
+            
+        }
+
+        return results;
+    }
+
+
 
 
 
